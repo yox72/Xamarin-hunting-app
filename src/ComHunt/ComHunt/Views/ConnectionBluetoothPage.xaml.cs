3b093c415fd19e9549ec06d9b760b3d2b45edda2
yox72 <yox72@live.fr>
2018-01-30 22:25:31 +0100

    BLUETOOTH FONCTIONNEL

--- src/ComHunt/ComHunt/Views/ConnectionBluetoothPage.xaml.cs
+++ src/ComHunt/ComHunt/Views/ConnectionBluetoothPage.xaml.cs
@@ -60,14 +60,15 @@ namespace ComHunt.Views
             }
             catch (DeviceConnectionException e)
             {
-                //... Couldn't connect to the device
+                await DisplayAlert("Alerte", "Problème de connexion", "OK");
             } 
         }
 
         private async void btnConnectKnow_Clicked (object sender, EventArgs lo){
             try
             {
-                await adapter.ConnectToKnownDeviceAsync(new Guid("guid"));
+                //await adapter.ConnectToKnownDeviceAsync(new Guid("guid"));
+                await adapter.ConnectToKnownDeviceAsync(new Guid("146F9EA1-1C21-BEAB-1B3B-72C78D5B9985"));
             }
             catch (DeviceConnectionException e)
             {
@@ -77,21 +78,22 @@ namespace ComHunt.Views
 
         IList<IService> services;
         IService service;
+        IList<ICharacteristic> characteristics;
+        ICharacteristic characteristic;
 
-        private async void btnGetServs_Clicked(object sender, EventArgs e){
+        /*private async void btnGetServs_Clicked(object sender, EventArgs e){
             services = await device.GetServicesAsync();
 
             service = await device.GetServiceAsync(
-                Guid.Parse("ffe0ecd2-3d16-4f8d-90de-e89e7fc396a5"));
+                //Guid.Parse("ffe0ecd2-3d16-4f8d-90de-e89e7fc396a5"));
+                Guid.Parse("0000ffe0-0000-1000-8000-00805f9b34fb"));
         }
-
-        IList<ICharacteristic> characteristics;
-        ICharacteristic characteristic;
 
         private async void btnGetCaracters_Clicked (object sender, EventArgs e){
             var characteristics = await service.GetCharacteristicsAsync();
             characteristic = await service.GetCharacteristicAsync(
-                Guid.Parse("d8de624e-140f-4a22-8594-e2216b84a5f2"));
+                //Guid.Parse("d8de624e-140f-4a22-8594-e2216b84a5f2"));
+                Guid.Parse("0000ffe1-0000-1000-8000-00805f9b34fb"));
         }
 
         private async void btnUpdate_Clicked (object sender, EventArgs e){
@@ -100,22 +102,69 @@ namespace ComHunt.Views
                 var bytes = args.Characteristic.Value;
             };
             await characteristic.StopUpdatesAsync();
-        }
+        }*/
 
-        private async void btnGetW_Clicked (object sender, EventArgs e){
-            //var bytes = await characteristic.ReadAsync();
-            byte[] byteText = Encoding.UTF8.GetBytes("Hello");
+        private async void btnChasseArret_Clicked (object sender, EventArgs e){
+            //services = await device.GetServicesAsync();
+            service = await device.GetServiceAsync(Guid.Parse("0000ffe0-0000-1000-8000-00805f9b34fb"));
+            characteristic = await service.GetCharacteristicAsync(Guid.Parse("0000ffe1-0000-1000-8000-00805f9b34fb"));
+            var data = await characteristic.ReadAsync();
+            //data[0] = "0,1,2,3,4,5,6;";
+            //await characteristic.WriteAsync(data);
+            //Fin changement
+            byte[] byteText = Encoding.UTF8.GetBytes("0,1,2,3,4,5,6;");
             await characteristic.WriteAsync(byteText);
         }
 
-        private async void btnGetR_Clicked(object sender, EventArgs e)
+        private async void btnSanglierVue_Clicked(object sender, EventArgs e)
         {
-            //var info = myConnection.thisCharacteristic.ReadAsync();
-            //var result = info.Result;
-            //string textresult = Encoding.UTF8.GetString(result);
+            service = await device.GetServiceAsync(Guid.Parse("0000ffe0-0000-1000-8000-00805f9b34fb"));
+            characteristic = await service.GetCharacteristicAsync(Guid.Parse("0000ffe1-0000-1000-8000-00805f9b34fb"));
+            byte[] byteText = Encoding.UTF8.GetBytes("1,1,0,0,0,0,0;");
+            await characteristic.WriteAsync(byteText);
         }
 
-        IList<IDescriptor> descriptors;
+        private async void btnSanglierTuer_Clicked(object sender, EventArgs e)
+        {
+            service = await device.GetServiceAsync(Guid.Parse("0000ffe0-0000-1000-8000-00805f9b34fb"));
+            characteristic = await service.GetCharacteristicAsync(Guid.Parse("0000ffe1-0000-1000-8000-00805f9b34fb"));
+            byte[] byteText = Encoding.UTF8.GetBytes("1,0,1,0,0,0,0;");
+            await characteristic.WriteAsync(byteText);
+        }
+
+        private async void btnChevreuilVue_Clicked(object sender, EventArgs e)
+        {
+            service = await device.GetServiceAsync(Guid.Parse("0000ffe0-0000-1000-8000-00805f9b34fb"));
+            characteristic = await service.GetCharacteristicAsync(Guid.Parse("0000ffe1-0000-1000-8000-00805f9b34fb"));
+            byte[] byteText = Encoding.UTF8.GetBytes("1,0,0,1,0,0,0;");
+            await characteristic.WriteAsync(byteText);
+        }
+
+        private async void btnChevreuilTuer_Clicked(object sender, EventArgs e)
+        {
+            service = await device.GetServiceAsync(Guid.Parse("0000ffe0-0000-1000-8000-00805f9b34fb"));
+            characteristic = await service.GetCharacteristicAsync(Guid.Parse("0000ffe1-0000-1000-8000-00805f9b34fb"));
+            byte[] byteText = Encoding.UTF8.GetBytes("1,0,0,0,1,0,0;");
+            await characteristic.WriteAsync(byteText);
+        }
+
+        private async void btnRenardVue_Clicked(object sender, EventArgs e)
+        {
+            service = await device.GetServiceAsync(Guid.Parse("0000ffe0-0000-1000-8000-00805f9b34fb"));
+            characteristic = await service.GetCharacteristicAsync(Guid.Parse("0000ffe1-0000-1000-8000-00805f9b34fb"));
+            byte[] byteText = Encoding.UTF8.GetBytes("1,0,0,0,0,1,0;");
+            await characteristic.WriteAsync(byteText);
+        }
+
+        private async void btnRenardTuer_Clicked(object sender, EventArgs e)
+        {
+            service = await device.GetServiceAsync(Guid.Parse("0000ffe0-0000-1000-8000-00805f9b34fb"));
+            characteristic = await service.GetCharacteristicAsync(Guid.Parse("0000ffe1-0000-1000-8000-00805f9b34fb"));
+            byte[] byteText = Encoding.UTF8.GetBytes("1,0,0,0,0,0,1;");
+            await characteristic.WriteAsync(byteText);
+        }
+
+        /*IList<IDescriptor> descriptors;
         IDescriptor descriptor;
 
         private async void btnDescriptor_Clicked (object sender, EventArgs e){
@@ -127,6 +176,6 @@ namespace ComHunt.Views
         private async void btnGetDescRW_Clicked (object sender, EventArgs e){
             var bytes = await descriptor.ReadAsync();
             await descriptor.WriteAsync(bytes);
-        }
+        }*/
     }
 }
